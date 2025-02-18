ec130fe96c70fc7b0cace34cd6e192dce3236721
dfolks <digitalfolks55@gmail.com>
2025-02-18 10:32:46 +0800

    complete profile.py and add more pages

--- pages/distribution.py
+++
@@ -1,6 +0,0 @@
-import streamlit as st
-
-
-if "df" in st.session_state:
-    st.write(st.session_state.df_name)
-    st.write(st.session_state.df)
--- pages/profile.py
+++ pages/profile.py
@@ -7,23 +7,43 @@ from pages.utils import dfprofiler
 # Description of app.
 st.title("Profile Dataset")
 
+with st.expander("Concepts"):
+    st.text(
+        """
+        The main purpose of data profiling is that to understand what your dataset looks like.
+        This tool helps you to understand the basics of your dataset.
+            - Data types (numbers and text) and key statistics (averages, count, max and min etc). 
+        It highlights potential problems, such as missing values or duplicate entries, which could cause issues in further data analysis.
+        The tool even shows you exactly where these problems are in your data.
+        This can help you to fix the issues quickly.
+
+        Data profiling on this tool.
+        1) Dataset samples
+            - You can check what kind of columns/data are in your dataset
+        2) Datatype and Statistical values of the data
+        3) Missing values
+        4) Duplicated values
+        """
+    )
+
 if "df" in st.session_state:
     df = st.session_state.df
-    st.subheader("Samples of dataset")
+    st.subheader("Dataset samples")
     st.dataframe(df.head())
 
-    st.subheader("Stastical values")
+    st.subheader("Datatype and Statistical values of the data")
     profiled_df = df.describe(include="all").loc[["count", "unique", "top", "freq", "mean", "std", "min", "max"]]
     profiled_df.loc["null (%)"] = df.isnull().sum()/len(df)*100
+    profiled_df.loc["Datatype"] = df.dtypes    
     st.dataframe(profiled_df)
     
-    st.subheader("Missing values (Null or Nan)")
+    st.subheader("Missing values")
     st.dataframe(df[df.isnull().any(axis=1)])
 
     st.subheader("Duplicated values")
-    st.dataframe(df[df.duplicated()])
+    st.dataframe(df[df.duplicated(keep=False)])
     
 
 else:
-    st.text("Please upload the file first")
+    st.text("Upload a file on the side bar")
 
